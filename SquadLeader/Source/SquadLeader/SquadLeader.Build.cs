2c7d3f73da7490d7eb25a106b82dbbaf916af087
daniel <daniel.dhaiby@hotmail.fr>
2021-01-27 16:59:20 -0500

    Made the first version of the unreal project

---
+++ SquadLeader/Source/SquadLeader/SquadLeader.Build.cs
@@ -0,0 +1,24 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class SquadLeader : ModuleRules
+{
+	public SquadLeader(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "AIModule", "GameplayTasks" });
+		PrivateDependencyModuleNames.AddRange(new string[] {  });
+
+		CppStandard = CppStandardVersion.Cpp17;
+
+		// Uncomment if you are using Slate UI
+		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+
+		// Uncomment if you are using online features
+		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+	}
+}
---
+++ SquadLeader/Source/SquadLeader/SquadLeader.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SquadLeader.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SquadLeader, "SquadLeader" );
