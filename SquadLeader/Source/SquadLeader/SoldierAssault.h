fed639a7125a37e5465c35f0c1e6c6af053c7823
daniel <daniel.dhaiby@hotmail.fr>
2021-01-27 21:14:31 -0500

    Added soldier classes

---
+++ SquadLeader/Source/SquadLeader/SoldierAssault.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Soldier.h"
+#include "SoldierAssault.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SQUADLEADER_API ASoldierAssault : public ASoldier
+{
+	GENERATED_BODY()
+	
+};
---
+++ SquadLeader/Source/SquadLeader/SoldierSupport.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Soldier.h"
+#include "SoldierSupport.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SQUADLEADER_API ASoldierSupport : public ASoldier
+{
+	GENERATED_BODY()
+	
+};
