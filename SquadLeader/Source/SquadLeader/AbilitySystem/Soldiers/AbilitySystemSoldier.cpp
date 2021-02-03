30dd95ebaa502e89b3f81756be9d73a82c5eb401
daniel <daniel.dhaiby@hotmail.fr>
2021-02-03 13:27:46 -0500

    Defined minimal classes for the ability system

---
+++ SquadLeader/Source/SquadLeader/AbilitySystem/Soldiers/AbilitySystemSoldier.cpp
@@ -0,0 +1,1 @@
+#include "AbilitySystemSoldier.h"
--- SquadLeader/Source/SquadLeader/Characters/Soldiers/SoldierPlayerState.h
+++ SquadLeader/Source/SquadLeader/Characters/Soldiers/SoldierPlayerState.h
@@ -1,5 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
@@ -13,5 +11,4 @@ UCLASS()
 class SQUADLEADER_API ASoldierPlayerState : public APlayerState
 {
 	GENERATED_BODY()
-	
 };
