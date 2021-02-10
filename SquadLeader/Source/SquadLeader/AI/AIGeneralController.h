7015a2d96d446da19a31ece1e91d166261fe6694
BenoitP <benoit.perucchietti@gmail.com>
2021-02-09 22:44:18 -0500

    Add the main classes for AI

---
+++ SquadLeader/Source/SquadLeader/AI/AIGeneralController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "AIGeneralController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SQUADLEADER_API AAIGeneralController : public AAIController
+{
+	GENERATED_BODY()
+	
+};
