bb989e46e680b7734fb4fbea94faf61853fc891c
personnedu13 <57003893+personnedu13@users.noreply.github.com>
2021-02-15 18:33:29 -0500

    Base

---
+++ SquadLeader/Source/SquadLeader/AreaEffect/AttributeSetAreaEffect.cpp
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AttributeSetAreaEffect.h"
+#include "GameplayEffect.h"
+#include "GameplayEffectExtension.h"
+
+void UAttributeSetAreaEffect::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+}
+
+bool UAttributeSetAreaEffect::PreGameplayEffectExecute(FGameplayEffectModCallbackData& Data)
+{
+	return false;
+}
+
+void UAttributeSetAreaEffect::PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue)
+{
+}
+
+void UAttributeSetAreaEffect::PostGameplayEffectExecute(const FGameplayEffectModCallbackData& Data)
+{
+}
---
+++ SquadLeader/Source/SquadLeader/AreaEffect/AttributeSetAreaEffect.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "Core.h"
+#include "UObject/ObjectMacros.h"
+#include "AttributeSet.h"
+#include "AbilitySystemComponent.h"
+#include "Net/UnrealNetwork.h"
+#include "AttributeSetAreaEffect.generated.h"
+
+#define ATTRIBUTE_ACCESSORS(ClassName, PropertyName) \
+	GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
+
+UCLASS()
+class SQUADLEADER_API UAttributeSetAreaEffect : public UAttributeSet
+{
+	GENERATED_BODY()
+	
+public:
+	UAttributeSetAreaEffect() = default;
+
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>&OutLifetimeProps) const override;
+	virtual bool PreGameplayEffectExecute(struct FGameplayEffectModCallbackData& Data) override;
+	virtual void PreAttributeChange(const FGameplayAttribute & Attribute, float& NewValue) override;
+	virtual void PostGameplayEffectExecute(const FGameplayEffectModCallbackData & Data) override;
+
+	// Duration
+	UPROPERTY(BlueprintReadOnly, Category = "Attributes | Duration", ReplicatedUsing = OnRep_Duration)
+	FGameplayAttributeData Duration;
+	ATTRIBUTE_ACCESSORS(UAttributeSetAreaEffect, Duration)
+
+protected:
+	UFUNCTION()
+	virtual void OnRep_Duration(const FGameplayAttributeData& OldValue);
+};
