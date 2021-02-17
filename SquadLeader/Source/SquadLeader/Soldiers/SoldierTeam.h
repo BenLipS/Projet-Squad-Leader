b88adff178770916b272e8d523be6a2e86ac5c3c
Thomas Ba <bait2201@usherbrooke.ca>
2021-02-17 11:54:40 -0500

    Create files for soldiers spawn and for team V2

---
+++ SquadLeader/Source/SquadLeader/Soldiers/SoldierTeam.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class SQUADLEADER_API SoldierTeam
+{
+public:
+	SoldierTeam();
+	~SoldierTeam();
+};
---
+++ SquadLeader/Source/SquadLeader/Spawn/SoldierSecondarySpawn.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "SoldierSpawn.h"
+#include "SoldierSecondarySpawn.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SQUADLEADER_API ASoldierSecondarySpawn : public ASoldierSpawn
+{
+	GENERATED_BODY()
+	
+};
