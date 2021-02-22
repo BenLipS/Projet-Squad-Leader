8bf5bfe67a41a88962363ae658cf4b5dd156a53a
daniel <daniel.dhaiby@hotmail.fr>
2021-02-22 11:37:55 -0500

    GEStateFighting: Duration is now 10 seconds

--- SquadLeader/Source/SquadLeader/AbilitySystem/Soldiers/GameplayEffects/States/GE_StateFighting.cpp
+++ SquadLeader/Source/SquadLeader/AbilitySystem/Soldiers/GameplayEffects/States/GE_StateFighting.cpp
@@ -1,6 +1,6 @@
 #include "GE_StateFighting.h"
 #include "../../../../Soldiers/Soldier.h"
 
-UGE_StateFighting::UGE_StateFighting() : UGE_State(ASoldier::StateFightingTag, 1.0f)
+UGE_StateFighting::UGE_StateFighting() : UGE_State(ASoldier::StateFightingTag, 10.f)
 {
 }
