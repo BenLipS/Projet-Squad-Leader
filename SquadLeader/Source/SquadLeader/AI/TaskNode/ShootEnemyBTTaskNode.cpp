6420955db0958d99646e90aab64ce0bde4fd8e30
Tommy <best3601@usherbrooke.com>
2021-03-04 15:06:58 -0500

    Addd the missing files

---
+++ SquadLeader/Source/SquadLeader/AI/TaskNode/ShootEnemyBTTaskNode.cpp
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ShootEnemyBTTaskNode.h"
+#include "../../AI/AIGeneralController.h"
+
+UShootEnemyBTTaskNode::UShootEnemyBTTaskNode() {
+	NodeName = "Shoot Enemy Task";
+	bNotifyTick = true;
+}
+
+EBTNodeResult::Type UShootEnemyBTTaskNode::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) {
+	AAIGeneralController* AIGeneralController = Cast<AAIGeneralController>(OwnerComp.GetOwner());
+
+	ResultState _result = AIGeneralController->ShootEnemy();
+	if (_result == ResultState::Success)
+		return EBTNodeResult::Succeeded;
+	if (_result == ResultState::Failed)
+		return EBTNodeResult::Failed;
+	return EBTNodeResult::InProgress;
+}
+
+void UShootEnemyBTTaskNode::TickTask(class UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds) {
+	AAIGeneralController* _controller = Cast<AAIGeneralController>(OwnerComp.GetOwner());
+
+	ResultState _result = _controller->ShootEnemy();
+	if (_result == ResultState::Success)
+		FinishLatentTask(OwnerComp, EBTNodeResult::Succeeded);
+	else if (_result == ResultState::Failed)
+		FinishLatentTask(OwnerComp, EBTNodeResult::Failed);
+}
+
+/** Permet de définir une description pour la tâche. C'est ce texte qui
+ apparaîtra dans le noeud que nous ajouterons au Behavior Tree */
+FString UShootEnemyBTTaskNode::GetStaticDescription() const
+{
+	return TEXT("Shoot on an enemy until he is dead");
+}
