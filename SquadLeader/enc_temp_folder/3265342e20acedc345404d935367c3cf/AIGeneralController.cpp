c35d02f5d510ee1d8d31a0ba4fd6e4cd8dc037f0
Benoit Perucchietti <Benoit.Perucchietti@USherbrooke.ca>
2021-03-05 09:51:24 -0500

    IsAlive to see actor

--- SquadLeader/enc_temp_folder/3265342e20acedc345404d935367c3cf/AIGeneralController.cpp
+++ SquadLeader/enc_temp_folder/3265342e20acedc345404d935367c3cf/AIGeneralController.cpp
@@ -36,7 +36,7 @@ void AAIGeneralController::ontargetperception_update_sight(AActor* actor, FAISti
 void AAIGeneralController::ActorsPerceptionUpdated(const TArray < AActor* >& UpdatedActors) {
 
 	for (auto& Elem : UpdatedActors) {
-		if(ASoldier* soldier = Cast<ASoldier>(Elem); soldier && soldier->IsAlive()){
+		if(ASoldier* soldier = Cast<ASoldier>(Elem); soldier){
 			if (SeenSoldier.Contains(soldier));
 			else SeenSoldier.Add(soldier);
 		}
