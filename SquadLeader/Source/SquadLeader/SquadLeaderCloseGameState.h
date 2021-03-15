c5a00b170b6a08e4da8440d9ad7313dc65af9c53
Thomas Ba <bait2201@usherbrooke.ca>
2021-03-15 15:21:03 -0400

    GameState: Create 3 new GameState.
    Create different GameState for different moments of the game:
    
    - *SquadLeaderInitGameState* for the begining, before the game start (initialize the different elements to launch the game).
    - *SquadLeaderGameState* for sharing Gamemode data during the game.
    - *SquadLeaderCloseGameState* that will be call when the game is closing.

---
+++ SquadLeader/Source/SquadLeader/SquadLeaderCloseGameState.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameStateBase.h"
+#include "SquadLeaderCloseGameState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SQUADLEADER_API ASquadLeaderCloseGameState : public AGameStateBase
+{
+	GENERATED_BODY()
+	
+};
