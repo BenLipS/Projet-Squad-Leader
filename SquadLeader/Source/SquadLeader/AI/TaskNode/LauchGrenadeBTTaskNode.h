c0a7341aea740b6dcfde67b06a1d99629d92ac5f
Benoit Perucchietti <Benoit.Perucchietti@USherbrooke.ca>
2021-03-26 17:19:35 -0400

    Add start launch nade task

---
+++ SquadLeader/Source/SquadLeader/AI/TaskNode/LauchGrenadeBTTaskNode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "LauchGrenadeBTTaskNode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SQUADLEADER_API ULauchGrenadeBTTaskNode : public UBTTaskNode
+{
+	GENERATED_BODY()
+	
+};
