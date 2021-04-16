e155a1117958acfc50869e44d0031cedc0257e99
Tommy <best3601@usherbrooke.com>
2021-04-16 16:03:49 -0400

    Test the navigation system - create an obstacle - still need to change the search of index to dychotomie search

---
+++ SquadLeader/Source/SquadLeader/AI/InfluenceMap/ObstacleActor.cpp
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ObstacleActor.h"
+#include "../../GameState/SquadLeaderGameState.h"
+#include "../../SquadLeaderGameModeBase.h"
+
+// Sets default values
+AObstacleActor::AObstacleActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AObstacleActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AObstacleActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	if (TickNumber % 2 == 0) {
+		auto GM = Cast<ASquadLeaderGameModeBase>(GetWorld()->GetAuthGameMode());
+		if (GM && GM->InfluenceMap) {
+			FGridPackage Package;
+			Package.m_location_on_map = GetActorLocation();
+			Package.team_value = 1;
+			Package.m_type = Type::Soldier;
+			GM->InfluenceMap->ReceivedMessage(Package);
+		}
+	}
+	TickNumber++; 
+}
+
