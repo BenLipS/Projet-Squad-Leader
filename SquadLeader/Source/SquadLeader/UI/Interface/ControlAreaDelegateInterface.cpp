2e6edde861dc0088e416fb08ef6040a717738774
personnedu13 <57003893+personnedu13@users.noreply.github.com>
2021-04-16 17:25:41 -0400

    En attente de Thomas

--- SquadLeader/Source/SquadLeader/UI/Interface/ControlAreaDelegateInterface.cpp
+++ SquadLeader/Source/SquadLeader/UI/Interface/ControlAreaDelegateInterface.cpp
@@ -7,17 +7,25 @@
 
 void IControlAreaDelegateInterface::AddControlAreaDelegate(IControlAreaInterface* newDelegate)
 {
+	ControlAreaDelegates.AddUnique(newDelegate);
 }
 
 void IControlAreaDelegateInterface::RemoveControlAreaDelegate(IControlAreaInterface* removeDelegate)
 {
+	ControlAreaDelegates.Remove(removeDelegate);
 }
 
 void IControlAreaDelegateInterface::OnControlAreaInit(unsigned int nbArea)
 {
-
+	for (auto ControlAreaInterface : ControlAreaDelegates)
+	{
+		ControlAreaInterface->OnControlAreaInit(nbArea);
+	}
 }
-void IControlAreaDelegateInterface::OnAreaCaptureChanged(unsigned int index, int owner, float capturePercent)
+void IControlAreaDelegateInterface::OnAreaCaptureChanged(unsigned int index, int owner, int capturer, float capturePercent)
 {
-	
+	for (auto ControlAreaInterface : ControlAreaDelegates)
+	{
+		ControlAreaInterface->OnAreaCaptureChanged(index, owner, capturer, capturePercent);
+	}
 }
