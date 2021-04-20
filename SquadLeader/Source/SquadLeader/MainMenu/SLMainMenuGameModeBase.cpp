cd3331efbc6eb1653e853a0b3b3ae4844f08404d
Thomas Ba <bait2201@usherbrooke.ca>
2021-04-20 11:03:36 -0400

    Free the mouse cursor on menu

--- SquadLeader/Source/SquadLeader/MainMenu/SLMainMenuGameModeBase.cpp
+++ SquadLeader/Source/SquadLeader/MainMenu/SLMainMenuGameModeBase.cpp
@@ -1,5 +1,22 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "SLMainMenuGameModeBase.h"
+#include "Blueprint/WidgetLayoutLibrary.h"
 
+
+void ASLMainMenuGameModeBase::StartPlay() {
+	for (auto PCIterator = GetWorld()->GetPlayerControllerIterator(); PCIterator; PCIterator++)
+	{
+		if (auto PC = PCIterator->Get(); PC)
+		{
+			PC->SetShowMouseCursor(true);
+			PC->ClientIgnoreMoveInput(true);
+			PC->ClientIgnoreLookInput(true);
+
+			FVector2D Center = UWidgetLayoutLibrary::GetViewportSize(GetWorld()) / 2.0f;
+			PC->SetMouseLocation(Center.X, Center.Y);
+		}
+	}
+
+	Super::StartPlay();
+}
