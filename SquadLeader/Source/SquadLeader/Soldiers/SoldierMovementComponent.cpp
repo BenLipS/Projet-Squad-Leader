80329bbdf28dc5db70edc2d213828d91025012f5
daniel <daniel.dhaiby@hotmail.fr>
2021-04-29 10:13:41 -0400

    Soldier: Movespeed has now a multiplier based on the direction (player and AI) + define the multiplier with a curve in character movement component

--- SquadLeader/Source/SquadLeader/Soldiers/SoldierMovementComponent.cpp
+++ SquadLeader/Source/SquadLeader/Soldiers/SoldierMovementComponent.cpp
@@ -9,52 +9,84 @@ USoldierMovementComponent::USoldierMovementComponent(const FObjectInitializer& _
 
 float USoldierMovementComponent::GetMaxSpeed() const
 {
-	ASoldier* soldier = Cast<ASoldier>(GetCharacterOwner());
+	ASoldier* Soldier = Cast<ASoldier>(GetCharacterOwner());
 
-	if (!soldier)
+	if (!Soldier)
 	{
 		UE_LOG(LogTemp, Error, TEXT("%s() No Soldier as owner"), *FString(__FUNCTION__));
 		return Super::GetMaxSpeed();
 	}
 
-	if (!soldier->IsAlive())
+	if (!Soldier->IsAlive())
 		return 0.f;
 
-	UAbilitySystemSoldier* ASC = soldier ? soldier->GetAbilitySystemComponent() : nullptr;
-	float finalSpeed = 0.f;
+	UAbilitySystemSoldier* ASC = Soldier ? Soldier->GetAbilitySystemComponent() : nullptr;
+	float FinalSpeed = 0.f;
 
 	switch (MovementMode)
 	{
 	case MOVE_Walking:
 	case MOVE_NavWalking:
+	{
+		const float AngleDirection = CalculateDirection(Soldier->GetVelocity(), Soldier->GetActorRotation());
+		const float MoveSpeedDirectionMultiplier = CurveMoveSpeedMultiplierDirection ? CurveMoveSpeedMultiplierDirection->GetFloatValue(AngleDirection) : 1.f;
+
 		if (IsCrouching())
-			finalSpeed = ASC ? ASC->GetNumericAttribute(UAttributeSetSoldier::GetMoveSpeedCrouchAttribute()) : MaxWalkSpeedCrouched;
+			FinalSpeed = ASC ? ASC->GetNumericAttribute(UAttributeSetSoldier::GetMoveSpeedCrouchAttribute()) : MaxWalkSpeedCrouched;
 		else
-			finalSpeed = ASC ? ASC->GetNumericAttribute(UAttributeSetSoldier::GetMoveSpeedWalkAttribute()) : MaxWalkSpeed;
+		{
+			FinalSpeed = ASC ? ASC->GetNumericAttribute(UAttributeSetSoldier::GetMoveSpeedWalkAttribute()) : MaxWalkSpeed;
+			GEngine->AddOnScreenDebugMessage(20, 2.f, FColor::Green, FString::Printf(TEXT("Direction: %f"), AngleDirection));
+		}
+
+		FinalSpeed *= MoveSpeedDirectionMultiplier;
 		break;
+	}
 
 	case MOVE_Falling:
-		finalSpeed = ASC ? ASC->GetNumericAttribute(UAttributeSetSoldier::GetMoveSpeedWalkAttribute()) : MaxWalkSpeed;
+		FinalSpeed = ASC ? ASC->GetNumericAttribute(UAttributeSetSoldier::GetMoveSpeedWalkAttribute()) : MaxWalkSpeed;
 		break;
 
 	case MOVE_Swimming:
-		finalSpeed = MaxSwimSpeed;
+		FinalSpeed = MaxSwimSpeed;
 		break;
 
 	case MOVE_Flying:
-		finalSpeed = MaxFlySpeed;
+		FinalSpeed = MaxFlySpeed;
 		break;
 
 	case MOVE_Custom:
-		finalSpeed = MaxCustomMovementSpeed;
+		FinalSpeed = MaxCustomMovementSpeed;
 		break;
 
 	case MOVE_None:
 	default:
-		finalSpeed = 0.f;
+		FinalSpeed = 0.f;
 		break;
 	}
 
-	const float moveSpeedMultiplier = ASC ? ASC->GetNumericAttribute(UAttributeSetSoldier::GetMoveSpeedMultiplierAttribute()) : 1.f;
-	return finalSpeed * moveSpeedMultiplier;
+	const float MoveSpeedMultiplier = ASC ? ASC->GetNumericAttribute(UAttributeSetSoldier::GetMoveSpeedMultiplierAttribute()) : 1.f;
+	return FinalSpeed * MoveSpeedMultiplier;
+}
+
+float USoldierMovementComponent::CalculateDirection(const FVector& _Velocity, const FRotator& _BaseRotation) const
+{
+	if (!_Velocity.IsNearlyZero())
+	{
+		const FMatrix RotMatrix = FRotationMatrix(_BaseRotation);
+		const FVector ForwardVector = RotMatrix.GetScaledAxis(EAxis::X);
+		const FVector RightVector = RotMatrix.GetScaledAxis(EAxis::Y);
+		const FVector NormalizedVel = _Velocity.GetSafeNormal2D();
+
+		// get a cos(alpha) of forward vector vs velocity
+		const float ForwardCosAngle = FVector::DotProduct(ForwardVector, NormalizedVel);
+		// now get the alpha and convert to degree
+		float ForwardDeltaDegree = FMath::RadiansToDegrees(FMath::Acos(ForwardCosAngle));
+
+		// depending on where right vector is, flip it
+		const float RightCosAngle = FVector::DotProduct(RightVector, NormalizedVel);
+
+		return (RightCosAngle < 0) ? -ForwardDeltaDegree : ForwardDeltaDegree;
+	}
+	return 0.f;
 }
