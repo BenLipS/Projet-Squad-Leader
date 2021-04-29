25965f6d41af8951987508a831ddef3983124ec9
daniel <daniel.dhaiby@hotmail.fr>
2021-04-29 08:50:45 -0400

    GA_LaunchGrenade.h: Put back missing function header

--- SquadLeader/Source/SquadLeader/AbilitySystem/Soldiers/GameplayAbilities/GA_LaunchGrenade.h
+++ SquadLeader/Source/SquadLeader/AbilitySystem/Soldiers/GameplayAbilities/GA_LaunchGrenade.h
@@ -20,6 +20,8 @@ protected:
 
 	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* OwnerInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
 
+	virtual void EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled) override;
+
 	UFUNCTION()
 	void MontageCompletedOrBlendedOut();
 
