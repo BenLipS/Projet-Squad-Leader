358dd9def536d8048baba3678e7a96e68c62894f
Thomas Ba <bait2201@usherbrooke.ca>
2021-05-06 15:46:49 -0400

    Add a name for each class

--- SquadLeader/Source/SquadLeader/MainMenu/PlayerParam/PlayerParam.h
+++ SquadLeader/Source/SquadLeader/MainMenu/PlayerParam/PlayerParam.h
@@ -4,6 +4,8 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Info.h"
+#include "../../Soldiers/AIs/SoldierAI.h"
+#include "../../Soldiers/Players/SoldierPlayer.h"
 #include "PlayerParam.generated.h"
 
 /**
@@ -15,5 +17,10 @@ class SQUADLEADER_API APlayerParam : public AInfo
 	GENERATED_BODY()
 public:
 	int TeamID = 0;  // 0 for null
-
+	TSubclassOf<ASoldierPlayer> PlayerClass;
+	TSubclassOf<ASoldierAI> ClassAI1;
+	TSubclassOf<ASoldierAI> ClassAI2;
+	TSubclassOf<ASoldierAI> ClassAI3;
+	TSubclassOf<ASoldierAI> ClassAI4;
+	TSubclassOf<ASoldierAI> ClassAI5;
 };
